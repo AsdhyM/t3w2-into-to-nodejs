7c1a466a43d22cfd0017e0ea7b1d2d45aab6b76c
Asdhy Piozzi <asdhyme@gmail.com>
2024-04-09 20:01:13 +1000

    Initialised nvm and installed pokemon node module

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Terminal app is running!");
+
+const pokemon = require('pokemon');
+
+let randomPokemon = pokemon.randon();
+
+console.log(`Today's lucky Pokemon is ${randomPokemon}`);
+
+console.log("Bye bye, terminal app finished");
